dab55d07a6bb8589a84f35f2f4083c00cbdddff9
portfolio-team <67832716+portfolio-team@users.noreply.github.com>
2020-08-05 16:53:39 +0900

    [add] #1 ヘッダー・トップビュー・worksコンポーネント追加 (#1)
    Co-authored-by: kakihiro <kid.akihiro@gmail.com>

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,23 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HttpClientModule }    from '@angular/common/http';
+import { DataService } from './data.service';
 import { ScullyLibModule } from '@scullyio/ng-lib';
+import { HeaderComponent } from './components/header/header.component';
+import { TopviewComponent } from './components/topview/topview.component';
+import { WorksComponent } from './components/works/works.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    TopviewComponent,
+    WorksComponent
   ],
   imports: [
     BrowserModule,
+    HttpClientModule,
     AppRoutingModule,
     ScullyLibModule
   ],
