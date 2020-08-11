027013deac0d85fc31f378eed8c8d77a3d78d151
portfolio-team <67832716+portfolio-team@users.noreply.github.com>
2020-08-11 16:39:52 +0900

    [fix] #3 worksコンポーネント編集 + workコンテンツのtemplateComponent編集+ scssのmixins・variables追加 (#3)
    Co-authored-by: kakihiro <kid.akihiro@gmail.com>

--- src/app/components/works/work-template2/work-template2.component.ts
+++ src/app/components/works/work-template2/work-template2.component.ts
@@ -7,9 +7,14 @@ import { Component, OnInit,Input } from '@angular/core';
 })
 export class WorkTemplate2Component implements OnInit {
   @Input() work:any
+  initialChar:String;
+  mainText:String;
+
   constructor() { }
 
   ngOnInit(): void {
+    this.initialChar = this.work.works_text.substr(0,1);
+    this.mainText = this.work.works_text.slice(1);
   }
 
 }
